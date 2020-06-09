b86eba3c765b2a0baca6284155a5944db0c8a70a
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-06-09 02:37:13 +0000

    Added route to hand post request from url/new

--- express_server.js
+++ express_server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+// helper function
+function generateRandomString() {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
 };
 
 app.get('/', (req, res) => {
@@ -26,7 +37,14 @@ app.get('/urls', (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-// http://localhost:8080/urls/b2xVn2
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+ });
+ // handles post request by url/new form
+ app.post('/urls', (req, res) => {
+  console.log(req.body);  //Log the POST request body to the console
+  res.send('OK');         // Respond with 'Ok' (we will replace this)
+ });
 
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
