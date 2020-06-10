4cc1faf5cce3871264225a6a8f3e606997052229
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-06-10 21:39:30 +0000

    Added GET /login endpoint and comments

--- express_server.js
+++ express_server.js
@@ -1,12 +1,9 @@
+// <---------------------------------- Constants -------------------------------------->
 const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,8 +12,14 @@ const urlDatabase = {
 
 const users = {};
 
-// helper function
-function generateRandomString() {
+// <---------------------------------- Middleware -------------------------------------->
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+// <------------------------------- Helper Functions ----------------------------------->
+
+const generateRandomString = function () {
   let result = '';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   for (let i = 0; i < 6; i++) {
@@ -24,6 +27,17 @@ function generateRandomString() {
   }
   return result;
 };
+
+const emailLookup = function (database, userEmail) {
+  for (const user in database) {
+    if (database[user].email === userEmail) {
+      return true;
+    }
+  }
+  return false;
+};
+
+// <-------------------------------- GET requests -------------------------------------->
 
 app.get('/', (req, res) => {
   res.send("Hello!");
@@ -39,6 +53,7 @@ app.get('/hello', (req, res) => {
 
 // renders urls page on GET request
 app.get('/urls', (req, res) => {
+  console.log(users);
   let templateVars = { urls: urlDatabase, user: users[req.cookies.user_id] };
   res.render("urls_index", templateVars);
 });
@@ -54,6 +69,10 @@ app.get('/register', (req, res) => {
   res.render('user_registration');
  });
 
+app.get('/login', (req, res) => {
+  return res.render('user_login');
+});
+
  // redirects to actual website from shortURL
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
@@ -65,6 +84,9 @@ app.get('/urls/:shortURL', (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies.user_id] };
   res.render('urls_show', templateVars);
 });
+
+
+// <------------------------------- POST requests -------------------------------------->
  
  // handles post request by url/new form, 
  //  generates shortURL updates database with long and short URLs
@@ -80,6 +102,15 @@ app.post('/register', (req, res) => {
   const userID = generateRandomString();
   const userEmail = req.body.email;
   const userPass = req.body.password;
+
+  if(!userID || !userPass) {
+    return res.status(400).send('Error 400: Email and/or Password Invalid');
+  }
+
+  if (emailLookup(users, userEmail)) {
+    return res.status(400).send('Error 400: Email already exists');
+  }
+
   const newUser = {
     id : userID,
     email : userEmail,
@@ -114,6 +145,7 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   res.redirect('/urls');
 });
 
+// <-------------------------------- Server Listen -------------------------------------->
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
